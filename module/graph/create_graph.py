ff6ae9cfad7e98858dffa529eb5e13ab5e2d7630
Jonathan Hsu <johnnhsu1031@gmail.com>
2024-02-09 14:11:31 -0800

    remove edge class

--- module/graph/create_graph.py
+++ module/graph/create_graph.py
@@ -1,6 +1,5 @@
 from node import Node
 from graph import Graph
-from edge import Edge
 
 
 # Create instances of the Graph, Nodes, and Edge
@@ -12,10 +11,6 @@ node_eth_exchange1 = Node("Exchange1", "ETH", 3000)
 
 crypto_graph.add_node(node_btc_exchange1)
 crypto_graph.add_node(node_eth_exchange1)
-
-# Example edge
-edge_btc_to_eth = Edge(node_btc_exchange1, node_eth_exchange1, fee=0.001, exchange_rate=20)
-crypto_graph.add_edge(edge_btc_to_eth)
 
 # Display the graph
 #print(crypto_graph)
